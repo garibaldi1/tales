a5b20eb9f3516e3073bcbc687e9434f20f1e2dd8
Alex <peresok@mail.ru>
2017-06-01 21:18:15 +0300

    Ajax функцилнал готов.

--- public_html/js/gettales.js
+++ public_html/js/gettales.js
@@ -8,3 +8,66 @@ $.ajax({
     }
 });
 }
+
+function getathors(){
+    $.ajax({
+       type:"post",
+       url:"php/dataauthors.php",
+       success: function(data){
+           $("#result").html(data);
+       }
+    });
+}
+
+function getstory(){
+    $.ajax({
+       type:"post",
+       url:"php/datastory.php",
+       success: function(data){
+           $("#result").html(data);
+       }
+    });
+}
+
+function getpoetry(){
+    $.ajax({
+        type:"post",
+        url:"php/datapoetry.php",
+        success: function(data){
+            $("#result").html(data);
+        }
+    });
+}
+
+function getfable(){
+    $.ajax({
+        type:"post",
+        url:"php/datafable.php",
+        success: function(data){
+            $("#result").html(data);
+        }
+    });
+}
+
+
+function getmyth(){
+    $.ajax({
+        type:"post",
+        url:"php/datamyth.php",
+        success: function(data){
+            $("#result").html(data);
+        }
+    });
+}
+
+
+function getaudio(){
+    $.ajax({
+        type:"post",
+        url:"php/dataaudio.php",
+        success: function(data){
+            $("#result").html(data);
+        }
+    });
+}
+
---
+++ public_html/php/dataauthors.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+require_once ("talesdb.php");
+$query = "SELECT*FROM tales";
+$select_tales = mysqli_query($link, $query);
+while($author = mysqli_fetch_array($select_tales)){
+    $id = $author['id'];
+    $name = $author['name'];
+    $author1 = $author['author'];
+    $ganre = $author['ganre'];
+    echo "<tr><td>$id</td><td>$name</td><td>$author1</td><td>$ganre</td></tr>";
+}
+?>
