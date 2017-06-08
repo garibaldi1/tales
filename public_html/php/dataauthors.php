2988b695a2bf5b9d1d1781ad03ecc635118a81cb
alex <peresok@mail.ru>
2017-06-08 17:07:06 +0300

    Подготовлена верстка под таблицу. Необходимо разработать таблицу в соответсвии с версткой

--- public_html/php/dataauthors.php
+++ public_html/php/dataauthors.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-type: text/html; charset=utf-8');
 require_once ("talesdb.php");
-$query = "SELECT*FROM tales";
+$query = "SELECT*FROM authors";
 $select_tales = mysqli_query($link, $query);
 while($author = mysqli_fetch_array($select_tales)){
     $id = $author['id'];
